b24c5cf599d62fde4b9bed9c0e6772b27b35a300
arctarus <arthur.dewarumez@gmail.com>
2016-06-17 18:34:31 +0200

    Add imports

---
+++ simple_chart_mixin.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+# indent-tabs-mode: 1
+# tab-width: 4
+
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import QPoint, Qt
+from PyQt5.QtGui import QPainter, QPen, QFontMetrics, QFont
